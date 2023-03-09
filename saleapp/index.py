fa2cddd5da7b9d8b0c25ca28c80718493e84ad14
Phát Bùi <buitienphat2462002@gmail.com>
2023-03-09 13:14:36 +0700

    add giao dien

--- saleapp/index.py
+++ saleapp/index.py
@@ -22,7 +22,15 @@ def chat_room():
 
     user_send = [untils.get_user_by_id(x.user_id).name for x in untils.load_message(room.room_id)]
 
+    user_image = [untils.get_user_by_id(x.user_id).avatar for x in untils.load_message(room.room_id)]
+
+    user_id = [x.user_id for x in untils.load_message(room.room_id)]
+
+    host_avatar = untils.get_host_room_avatar(room.room_id);
+
     user_send.pop(0)
+    user_image.pop(0)
+    user_id.pop(0)
 
     print(user_send)
 
@@ -31,7 +39,8 @@ def chat_room():
         print(untils.load_message(room.room_id)[0].content)
         return render_template('chatroom.html', user_name=user_name, room=room.room_id, name= current_user.name,
                                message=untils.load_message(room.room_id), room_id = int(room.room_id),
-                               user_send= user_send, n=len(user_send))
+                               user_send= user_send, n=len(user_send), user_image=user_image, user_id=user_id, room_name = untils.get_chatroom_by_id(room.room_id),
+                               host_avatar=host_avatar);
     else:
         return redirect(url_for('home'))
 
@@ -40,20 +49,29 @@ def chat_room():
 def chat_room_admin(room_id):
 
     if current_user.user_role == UserRole.ADMIN:
-        print(room_id)
+        # print(room_id)
         user_name = current_user.name
         room = untils.get_chatroom_by_room_id(id=room_id)
 
         user_send = [untils.get_user_by_id(x.user_id).name for x in untils.load_message(room.room_id)]
 
+        user_image = [untils.get_user_by_id(x.user_id).avatar for x in untils.load_message(room.room_id)]
+
+        user_id = [x.user_id for x in untils.load_message(room.room_id)]
+
+        host_avatar = untils.get_host_room_avatar(room_id);
+
         user_send.pop(0)
+        user_image.pop(0)
+        user_id.pop(0)
+
 
         if user_name and room:
             print(untils.load_message(room.room_id)[0].content)
             return render_template('chatroom.html', user_name=user_name, room=room.room_id, name=current_user.name,
                                    message=untils.load_message(room.room_id), room_id=int(room.room_id),
-                                   user_send=user_send, n=len(user_send))
-
+                                   user_send=user_send, n=len(user_send), user_image=user_image, user_id=user_id,
+                                   room_name=untils.get_chatroom_by_id(room.room_id), host_avatar=host_avatar);
     return redirect(url_for('home'))
 
 
@@ -62,6 +80,8 @@ def handle_send_message_event(data):
     app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
                                                                     data['room'],
                                                                     data['message']))
+
+    app.logger.info("{}".format(data['user_avatar']))
     socketio.emit('receive_message', data, room=data['room'])
 
 @socketio.on('save_message')
