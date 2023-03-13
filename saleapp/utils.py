0babf8bd13f8f239917d5e6b5c3346227b236c94
Genji-kun <87120829+Genji-kun@users.noreply.github.com>
2023-03-13 18:19:44 +0700

    phat

--- saleapp/utils.py
+++ saleapp/utils.py
@@ -1,3 +1,7 @@
+import math
+from random import random
+
+from saleapp import app
 
 
 def cart_stats(list):
@@ -11,3 +15,22 @@ def cart_stats(list):
         'total_price': total_price,
         'total_quantity': total_quantity
     }
+
+from random import random
+
+
+def generateOTP():
+    # Declare a string variable
+    # which stores all string
+    string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    OTP = ""
+    length = len(string)
+    for i in range(6):
+        OTP += string[math.floor(random() * length)]
+
+    return OTP + ""
+
+
+# if __name__ == '__main__':
+#     with app.app_context():
+#         print(generateOTP())
